e785b1b18b7547aec030c1255c38dd5e8dca672e
iansmellis <iansmellis@gmail.com>
2016-04-15 13:10:48 -0500

    Added Item_Master to handle the items and Item_Throw to throw the item in hand.

---
+++ Assets/GTGD S3/_Scripts/Item Scripts/Item_Throw.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace S3
+{
+    public class Item_Throw : MonoBehaviour
+    {
+        Item_Master itemMaster;
+        Transform myTransform;
+        Rigidbody myRigidbody;
+        Vector3 throwDirection;
+
+        public bool canBeThrown;
+        public string throwButtonName;
+        public float throwForce;
+
+        void Start()
+        {
+            SetInitialReferences();
+        }
+
+        void Update()
+        {
+            CheckForThrowInput();
+        }
+
+        void SetInitialReferences()
+        {
+            itemMaster = GetComponent<Item_Master>();
+            myTransform = transform;
+            myRigidbody = GetComponent<Rigidbody>();
+        }
+
+        void CheckForThrowInput()
+        {
+            if(throwButtonName != null)
+            {
+                if(Input.GetButtonDown(throwButtonName) && Time.timeScale > 0 && myTransform.root.CompareTag(GameManager_References._playerTag))
+                {
+                    CarryOutThrowActions();
+                }
+            }
+        }
+
+        void CarryOutThrowActions()
+        {
+            throwDirection = myTransform.parent.forward;
+            myTransform.parent = null;
+            itemMaster.CallEventObjectThrow();
+            HurlItem();
+        }
+
+        void HurlItem()
+        {
+            myRigidbody.AddForce(throwDirection * throwForce,ForceMode.Impulse);
+        }
+    }
+}
--- Assets/GTGD S3/_Scripts/Master Scripts/Item_Master.cs
+++ Assets/GTGD S3/_Scripts/Master Scripts/Item_Master.cs
@@ -5,30 +5,53 @@ namespace S3
 {
     public class Item_Master : MonoBehaviour
     {
+        Player_Master playerMaster;
+        
+        public delegate void GeneralEventHandler();
+        public event GeneralEventHandler EventObjectThrow;
+        public event GeneralEventHandler EventObjectPickup;
+
+        public delegate void PickupActionEventHandler(Transform item);
+        public event PickupActionEventHandler EventPickupAction;
 
         void OnEnable()
         {
-
+            SetInitialReferences();
         }
 
-        void OnDisable()
+        public void CallEventObjectThrow()
         {
-
+            if(EventObjectThrow != null)
+            {
+                EventObjectThrow();
+                playerMaster.CallEventHandsEmpty();
+                playerMaster.CallEventInventoryChanged();
+            }
         }
 
-        void Start()
+        public void CallEventObjectPickup()
         {
-
+            if (EventObjectPickup != null)
+            {
+                EventObjectPickup();
+                playerMaster.CallEventInventoryChanged();
+            }
         }
 
-        void Update()
+        public void CallEventPickupAction(Transform item)
         {
-
+            if(EventPickupAction != null)
+            {
+                EventPickupAction(item);
+            }
         }
 
         void SetInitialReferences()
         {
-
+            if(GameManager_References._player != null)
+            {
+                playerMaster = GameManager_References._player.GetComponent<Player_Master>();
+            }
         }
     }
 }
